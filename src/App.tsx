aa144108f24a6f59d717e62ff8584108a99f51a1
merhmood <mahmudsuberu9@gmail.com>
2024-07-19 18:06:33 +0100

    feat: added bet placing logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,15 @@
-import GameOutcomeFactory from "./components/GameOutcomeFactory";
+/**
+ * Welcome the ROCK PAPER SCISSORS game
+ * - To place bet click on the box showing bet value.
+ * - Bet can only be a multiple of 500
+ */
+
+import { useState } from "react";
+import GameOutcomeFactory from "./components/factories/GameOutcomeFactory";
+import {
+  ClearGameControlButton,
+  PlayGameControlButton,
+} from "./components/HOC/withGameControl";
 
 enum GameChoices {
   ROCK = "ROCK",
@@ -12,31 +23,68 @@ enum GameOutcome {
   DRAW = "DRAW",
 }
 
+const BET_INCREMENT = 500;
+
 function App() {
-  const indicators = {
+  const [indicators, setIndicators] = useState({
     balance: 5000,
     bet: 0,
     win: 0,
-  };
+  });
 
-  const gameBets = {
+  const [gameBets, setGameBets] = useState({
     rock: { title: GameChoices.ROCK, amount: 0 },
-    paper: { title: GameChoices.PAPER, amount: 500 },
+    paper: { title: GameChoices.PAPER, amount: 0 },
     scissors: { title: GameChoices.SCISSORS, amount: 0 },
-  };
+  });
+
+  const [betPositions, setbetPositions] = useState<Array<string>>([]);
 
   const gamePlayed = false;
 
-  const gameOutcome = GameOutcome.WON;
+  const gameOutcome = "LOST";
 
   const choices = {
-    player: GameChoices.PAPER,
-    computer: GameChoices.SCISSORS,
+    player: null,
+    computer: null,
   };
 
   const wonChoice = GameChoices.PAPER;
 
   const objectToList = (object: object) => Object.entries(object);
+
+  const handleBetPlacing = (gameBet: { title: string; amount: number }) => {
+    if (indicators.balance > 0) {
+      // Place bet in choice
+      setGameBets((prevState: any) => ({
+        ...prevState,
+        [gameBet.title.toLocaleLowerCase()]: {
+          title: prevState[gameBet.title.toLocaleLowerCase()].title,
+          amount:
+            prevState[gameBet.title.toLocaleLowerCase()].amount + BET_INCREMENT,
+        },
+      }));
+      // Reduce player balance
+      setIndicators((prevState) => ({
+        ...prevState,
+        balance: prevState.balance - BET_INCREMENT,
+      }));
+    }
+    console.log(betPositions);
+    if (
+      betPositions.includes(gameBet.title.toLocaleLowerCase()) ||
+      betPositions.length <= 2
+    ) {
+      setbetPositions((prevState) => {
+        if (
+          !prevState.includes(gameBet.title.toLocaleLowerCase()) &&
+          prevState.length <= 2
+        )
+          return [...prevState, gameBet.title.toLocaleLowerCase()];
+        else return [...prevState];
+      });
+    }
+  };
 
   return (
     <main>
@@ -52,7 +100,7 @@ function App() {
         }
       </header>
       <section className="game-outcome">
-        {gameOutcome === "WON" &&
+        {gameOutcome === GameOutcome.WON &&
           GameOutcomeFactory.createGameOutcomeComponent("WON", wonChoice)}
       </section>
       <section className="game-choices">
@@ -67,21 +115,29 @@ function App() {
       <section className="game-board">
         <p className="game-board_title">PICK YOUR POSITIONS</p>
         <div className="game-board_pieces">
-          {objectToList(gameBets).map(([gameBet, value], index) => (
-            <div className={`game-board_bets ${gameBet}`} key={index}>
-              <p>{value.amount}</p>
-              <h2>{value.title}</h2>
-            </div>
-          ))}
+          {
+            // gameBet represents possible betting position
+            objectToList(gameBets).map(([gameBet, value], index) => (
+              <div
+                className={`game-board_bets ${gameBet}`}
+                key={index}
+                onClick={() =>
+                  handleBetPlacing({ title: value.title, amount: value.amount })
+                }
+              >
+                <p>{value.amount}</p>
+                <h2>{value.title}</h2>
+              </div>
+            ))
+          }
         </div>
-        {gamePlayed ? (
-          <button className="game-control">CLEAR</button>
-        ) : (
-          <button className="game-control">PLAY</button>
-        )}
+        {changeControlButton(gamePlayed)}
       </section>
     </main>
   );
 }
+
+const changeControlButton = (gamePlayed: boolean) =>
+  gamePlayed ? <ClearGameControlButton /> : <PlayGameControlButton />;
 
 export default App;
